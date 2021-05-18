2e1da21fb0483193736a1a937fe083ad1982ebc7
xeroman.k <xeroman.k@itsmart.co.kr>
2021-05-18 14:08:25 +0900

    project init

---
+++ src/main/java/com/itsm/dranswer/DranswerApplication.java
@@ -0,0 +1,13 @@
+package com.itsm.dranswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DranswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DranswerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsm/dranswer/DranswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsm.dranswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DranswerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

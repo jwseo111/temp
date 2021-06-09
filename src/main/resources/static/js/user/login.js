34c12181621a57aa45778838cabbb46f03f30499
xeroman.k <xeroman.k@itsmart.co.kr>
2021-06-09 17:20:44 +0900

    login tid code 수정

--- src/main/resources/static/js/user/login.js
+++ src/main/resources/static/js/user/login.js
@@ -1,6 +1,6 @@
 var appMain;
 var TID = {
-    LOGIN : {value : 0, name : "login", code : "L"}
+    LOGIN : {value : 0, name : "login", code : "S"}
 };
 window.onload = function(){
     appMain = new Vue({

426b3f14193cb92d31b29c3ddf67d42ee7469081
xeroman.k <xeroman.k@itsmart.co.kr>
2021-06-14 18:13:36 +0900

    초기적재데이터 적재 관련 기능 추가

---
+++ src/main/java/com/itsm/dranswer/commons/InitializeDataService.java
@@ -0,0 +1,73 @@
+package com.itsm.dranswer.commons;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itsm.dranswer.users.AgencyInfo;
+import com.itsm.dranswer.users.AgencyInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class InitializeDataService implements CommandLineRunner {
+
+    @Autowired
+    private AgencyInfoRepo agencyInfoRepo;
+
+    private final String FILE_INIT_AGENCY = "init/init_agency.json";
+
+    @Override
+    public void run(String... args) throws Exception {
+        initAgencyFromFile();
+    }
+
+    private void initAgencyFromFile() throws IOException {
+        List<AgencyInfo> agencyList = new ArrayList<>();
+        try (InputStream is = getStreamFromResource(FILE_INIT_AGENCY)) {
+            JsonNode jsonNode = getJsonNode(is, "agencyInfo");
+            agencyList = getAgencyListFromNode(jsonNode);
+        }
+
+        for(AgencyInfo agencyInfo : agencyList){
+            agencyInfoRepo.save(agencyInfo);
+        }
+    }
+
+    private InputStream getStreamFromResource(String fileLocation) {
+        ClassLoader classLoader = InitializeDataService.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileLocation);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("init data file \"" + fileLocation + "\" not found.");
+        } else {
+            return inputStream;
+        }
+    }
+
+    private JsonNode getJsonNode(InputStream is, String key) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try (DataInputStream dis = new DataInputStream(is)) {
+            jsonNode = objectMapper.readTree(dis).path(key);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        return jsonNode;
+    }
+
+    private List<AgencyInfo> getAgencyListFromNode(JsonNode jsonNode) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<AgencyInfo> agencyList = objectMapper.convertValue(jsonNode, new TypeReference<List<AgencyInfo>>() {
+        }).stream().collect(Collectors.toList());
+        return agencyList;
+    }
+
+}

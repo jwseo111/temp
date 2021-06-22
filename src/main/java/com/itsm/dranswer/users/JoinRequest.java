094e77e1fdab0c0657c9edbe531f90a712156656
xeroman.k <xeroman.k@itsmart.co.kr>
2021-06-22 13:08:09 +0900

    회원가입 요청 객체 주석추가

--- src/main/java/com/itsm/dranswer/users/JoinRequest.java
+++ src/main/java/com/itsm/dranswer/users/JoinRequest.java
@@ -30,10 +30,13 @@ public class JoinRequest {
     @NotBlank(message = "패스워드를 입력하세요.")
     private String inputPw;
 
+    // 권한 Role.java 참고
     private String userRole;
 
+    // 기관번호
     private Integer agencySeq;
 
+    // 질병코드 Disease.java 참고
     private String diseaseCode;
 
     @Size(message = "사용자 이름의 최대 길이를 초과 하였습니다.(max:20)", max = 20)
@@ -42,16 +45,22 @@ public class JoinRequest {
 
     private String userPhoneNumber;
 
+    // 질병 책임자 여부
     private String diseaseManagerYn;
 
+    // 네이버클라우드 ID
     private String nCloudId;
 
+    // 네이버클라우드AccessKey
     private String nCloudAccessKey;
 
+    // 네이버클라우드SecretKey
     private String nCloudSecretKey;
 
+    // 가입상태코드
     private String joinStatCode;
 
+    // 상위회원번호
     private Long parentUserSeq;
 
 }

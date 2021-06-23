e4cbd0f56188e2ab65c2d9a39478be69973b1a99
xeroman.k <xeroman.k@itsmart.co.kr>
2021-06-23 15:23:33 +0900

    주석 추가

--- src/main/java/com/itsm/dranswer/users/AgencyRestCtrl.java
+++ src/main/java/com/itsm/dranswer/users/AgencyRestCtrl.java
@@ -3,12 +3,11 @@ package com.itsm.dranswer.users;
 /*
  * @package : com.itsm.dranswer.users
  * @name : AgencyRestCtrl.java
- * @date : 2021-06-17 오후 2:54
+ * @date : 2021-06-23 오후 2:25
  * @author : xeroman.k
  * @version : 1.0.0
  * @modifyed :
  */
-
 
 import com.itsm.dranswer.utils.ApiUtils.ApiResult;
 import org.slf4j.Logger;
@@ -29,17 +28,43 @@ public class AgencyRestCtrl {
 
     private final AgencyService agencyService;
 
+    /**
+     * 
+     * @methodName : AgencyRestCtrl
+     * @date : 2021-06-23 오후 2:26
+     * @author : xeroman.k 
+     * @param agencyService
+     * @return : 
+     * @throws 
+     * @modifyed :
+     *
+    **/
     @Autowired
     public AgencyRestCtrl(AgencyService agencyService){
         this.agencyService = agencyService;
     }
 
+    /**
+     *
+     * @methodName : getAgencyList
+     * @date : 2021-06-23 오후 2:26
+     * @author : xeroman.k
+     * @param agencyTypeCode
+     * @param agencyName
+     * @param pageable
+     * @return : com.itsm.dranswer.utils.ApiUtils.ApiResult<org.springframework.data.domain.Page<com.itsm.dranswer.users.AgencyInfoDto>>
+     * @throws
+     * @modifyed :
+     *
+    **/
     @GetMapping(value = "/agency/list")
-    public ApiResult<Page<AgencyInfoDto>> getAgencyList(@RequestParam(required = false) AgencyType agencyTypeCode,
-                                         @RequestParam(required = false, defaultValue = "") String agencyName,
-                                         Pageable pageable){
+    public ApiResult<Page<AgencyInfoDto>> getAgencyList(
+            @RequestParam(required = false) AgencyType agencyTypeCode,
+            @RequestParam(required = false, defaultValue = "") String agencyName,
+            Pageable pageable){
 
-        Page<AgencyInfoDto> pageAgency = agencyService.getAgencyList(agencyTypeCode, agencyName, pageable);
+        Page<AgencyInfoDto> pageAgency =
+                agencyService.getAgencyList(agencyTypeCode, agencyName, pageable);
 
         return success(pageAgency);
     }

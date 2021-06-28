9f85f72fa9d5817149a912ff75d0873002ccc7ee
xeroman.k <xeroman.k@itsmart.co.kr>
2021-06-28 16:45:10 +0900

    add @Repository

--- src/main/java/com/itsm/dranswer/storage/BucketInfoRepo.java
+++ src/main/java/com/itsm/dranswer/storage/BucketInfoRepo.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OpenStorageInfoRepo extends JpaRepository<OpenStorageInfo, String> {
-
+public interface BucketInfoRepo extends JpaRepository<BucketInfo, String> {
 }

d71270d6331a3e4c69ebc4931ee80e2a45639cc1
leesunmin83 <awear83@gmail.com>
2021-06-30 14:06:45 +0900

    공통으로 사용가능한 함수추가 ( regExp, isNull, isValid, openPopupAgency, callbackPopupAgency)

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -86,8 +86,90 @@ Number.prototype.format = function(){
 };
 
 function getCodeList(codeId, callback){
+
     get(codeId,
         "/get/code/"+codeId,
         {},
         callback);
+}
+
+
+
+// 정규식
+function regExp(id, str){
+
+    const passExp = /^[a-zA-Z0-9_\!\@\#\$\%\^\&\*\?]{10,15}$/;   //pass
+    //const pass2Exp = /(?=.*\d{1,15})(?=.*[~`!@#$%\^&*()-+=]{1,15})(?=.*[a-zA-Z]{1,15}).{10,15}$/;
+    const pass2Exp = /(?=.*\d{1,15})(?=.*[a-zA-Z]{1,15}).{10,15}$/;
+
+    const emailExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i; // 이메일
+    let rtnMsg="";
+    if(id === "EMAIL"){
+        if(!emailExp.test(str)){
+            rtnMsg ="N";
+        }
+    }else if(id === "PASS"){
+
+        if(!passExp.test(str) && str !== ""){
+            rtnMsg ="10~15자 이내 영문, 숫자, 특수문자만 입력 가능합니다.";
+        }
+    }else if(id === "PASS2"){
+
+        if(!pass2Exp.test(str) && str !== ""){
+            rtnMsg ="10~15자 영문, 숫자, 특문 각 1회 이상 입력 가능합니다.";
+        }
+    }
+
+    return rtnMsg;
+
+}
+
+
+// 빈값 체크 함수
+function isNull(val){
+    if (val==="" || val===null || val===undefined)
+        return true;
+    else
+        return false;
+}
+
+// Validation check 함수
+// value - 항목의 값, title - 항목의 필드명, ref - ref or id  항목으로 focus, msg - 체크 메세지, type - S : radio,selectbox
+//ex) let param =[  {value:this.info.agencyTypeCode, title:"회원구분", ref:this.$refs.agencyTypeCode, type:"S", msg:""},]
+//    if(!isValid(param)) return false;
+function isValid(item){
+    for(let i=0;i < item.length;i++){
+        let type = item[i].type;
+        let afterText = "은(는) 필수입니다.";
+        let msg = item[i].msg;
+
+        if(isNull(msg)){
+            if(!isNull(type)){
+                if("S" === type){
+                    afterText = "을(를) 선택해주세요";
+                }
+            }
+            msg = item[i].title + afterText;
+        }
+
+        if(isNull(item[i].value)){
+            alert(msg);
+            if(!isNull(item[i].ref)){
+                item[i].ref.focus();
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+// 기관 팝업 open
+function openPopupAgency(code){
+    window.open("/popup/agency?agencyTypeCode="+code, "pop", "width=500px,height=500px");
+}
+
+// 기관명 팝업 callback
+
+function callbackPopupAgency(item){
+    appMain.$refs.maincontents.callbackPopupAgency(item);
 }

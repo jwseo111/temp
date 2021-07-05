ad07d588dff827c1ea4ed2e4c35f0737170a0028
lsj <suuujin.lee@gmail.com>
2021-07-05 17:21:53 +0900

    마이페이지 권한별 메뉴 수정

--- src/main/resources/static/js/common/mypage.js
+++ src/main/resources/static/js/common/mypage.js
@@ -21,17 +21,23 @@ Vue.component('mypagemenus', {
             menus_admin : [ //관리자
                 {name: "회원관리", uri: "", use: true},
                 {name: "저장신청관리", use: true},
-                {name : "공개신청 관리", uri:"", use : true},
-                {name : "학습데이터신청 관리", uri:"", use : true},
-                {name : "학습환경신청 관리", uri:"", use : true}
+                {name: "공개신청 관리", uri:"", use : true},
+                {name: "학습데이터신청 관리", uri:"", use : true},
+                {name: "학습환경신청 관리", uri:"", use : true}
             ],
             menus_uploader : [ // #####
                 {name: "저장신청 데이터 보기", uri: "/my/store/list", use: true},
                 {name: "질환데이터 업로드", use: true},
-                {name : "공개신청 데이터 보기", uri:"/my/store/list", use : true}
+                {name: "공개신청 데이터 보기", uri:"/my/store/list", use : true}
             ],
-            messages : "",
+            menus_user : [ // 기업
+                {name: "데이터 사용신청 보기", uri: "", use: true},
+                {name: "학습환경 사용신청 보기", use: true},
+                {name: "회원정보변경", uri:"", use : true}
+            ],
+            messages : ""
         };
+
     },
     mounted:function(){
     },

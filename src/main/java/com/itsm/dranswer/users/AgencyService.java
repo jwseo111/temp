44de4be35cdc1837ef5083424455009e3c0d949f
xeroman.k <xeroman.k@itsmart.co.kr>
2021-07-07 16:09:58 +0900

    add @transactional

--- src/main/java/com/itsm/dranswer/users/AgencyService.java
+++ src/main/java/com/itsm/dranswer/users/AgencyService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class AgencyService {
 

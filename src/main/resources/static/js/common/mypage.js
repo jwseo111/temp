9edc4a96f686d246e4be4132a260505557ea5e69
leesunmin83 <awear83@gmail.com>
2021-07-08 16:11:09 +0900

    마이페이지 메뉴 경로 수정(회원정보변경)

--- src/main/resources/static/js/common/mypage.js
+++ src/main/resources/static/js/common/mypage.js
@@ -16,7 +16,7 @@ Vue.component('mypagemenus', {
                 {name: "질환데이터 업로드", use: true},
                 {name: "공개신청 데이터 보기", uri:"/my/store/list", use : true},
                 {name: "기업요청 데이터 보기", uri:"", use : true},
-                {name: "회원정보변경", uri:"", use : true}
+                {name: "회원정보변경", uri:"/my/userModify", use : true}
                 ],
             menus_admin : [ //관리자
                 {name: "회원관리", uri: "/my/admin/memberList", use: true},
@@ -24,7 +24,7 @@ Vue.component('mypagemenus', {
                 {name: "공개신청 관리", uri:"", use : true},
                 {name: "학습데이터신청 관리", uri:"", use : true},
                 {name: "학습환경신청 관리", uri:"", use : true},
-                {name: "회원정보변경", uri:"/", use : true}
+                {name: "회원정보변경", uri:"/my/userModify", use : true}
             ],
             menus_uploader : [ // #####
                 {name: "저장신청 데이터 보기", uri: "/my/store/list", use: true},
@@ -34,7 +34,7 @@ Vue.component('mypagemenus', {
             menus_user : [ // 기업
                 {name: "데이터 사용신청 보기", uri: "", use: true},
                 {name: "학습환경 사용신청 보기", use: true},
-                {name: "회원정보변경", uri:"", use : true}
+                {name: "회원정보변경", uri:"/my/userModify", use : true}
             ],
             messages : ""
         };

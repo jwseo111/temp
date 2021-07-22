e0f59f0b3eb4aefd580899fa24254938c7d61aa3
leesunmin83 <awear83@gmail.com>
2021-07-22 15:12:36 +0900

    회원정보변경 특정 항목 Null에대한 예외처리 추가

--- src/main/resources/static/js/user/myUserModify.js
+++ src/main/resources/static/js/user/myUserModify.js
@@ -79,17 +79,28 @@ Vue.component('maincontents', {
             get(TID.INFO, "/user/my/info", null,this.callback);
         },
         getUserInfoCallback : function(results){
+
             let item = results.response;
-            this.info.agencyName = item.agencyInfo.agencyName;
-            this.info.agencyTypeCode = item.agencyInfo.agencyTypeCode.name;
-            this.info.agencyTypeName = item.agencyInfo.agencyTypeCode.desc;
-            this.info.blNumber = item.agencyInfo.blNumber;
-            this.info.ceoName = item.agencyInfo.ceoName;
-            this.info.diseaseCode = item.diseaseCode.name;
-            this.info.diseaseName = item.diseaseCode.desc;
+
+            if(!isNull(item.agencyInfo)){
+                this.info.agencyName = item.agencyInfo.agencyName;
+                this.info.agencyTypeCode = item.agencyInfo.agencyTypeCode.name;
+                this.info.agencyTypeName = item.agencyInfo.agencyTypeCode.desc;
+                this.info.blNumber = item.agencyInfo.blNumber;
+                this.info.ceoName = item.agencyInfo.ceoName;
+            }
+
+            if(!isNull(item.diseaseCode)){
+                this.info.diseaseCode = item.diseaseCode.name;
+                this.info.diseaseName = item.diseaseCode.desc;
+            }
+
             this.info.diseaseManagerYn = item.diseaseManagerYn;
-            this.info.joinStatCode = item.joinStatCode.name;
-            this.info.joinStatName = item.joinStatCode.desc;
+            if(!isNull(item.joinStatCode)){
+                this.info.joinStatCode = item.joinStatCode.name;
+                this.info.joinStatName = item.joinStatCode.desc;
+            }
+
             this.info.nCloudAccessKey = item.nCloudAccessKey;
             this.info.nCloudId = item.nCloudId;
             this.info.nCloudObjStorageId = item.nCloudObjStorageId;

2da45ce9fbe9640c55ad616c1a4ef0dc4d2ccc1f
leesunmin83 <awear83@gmail.com>
2021-08-09 12:32:16 +0900

    마이페이지 로그인 체크 로직 추가

--- src/main/java/com/itsm/dranswer/users/UserPage.java
+++ src/main/java/com/itsm/dranswer/users/UserPage.java
@@ -11,6 +11,7 @@ package com.itsm.dranswer.users;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -38,12 +39,14 @@ public class UserPage {
     }
 
     // 마이페이지(관리자)  > 회원목록
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/my/admin/memberList")
     public String myAdminMemberList() {
 
         return "pages/user/myMemberList";
     }
     // 마이페이지(관리자)  > 회원상세
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/my/admin/memberView")
     public String myAdminMemberView() {
         return "pages/user/myMemberView";
@@ -51,18 +54,21 @@ public class UserPage {
 
 
     // 마이페이지  > 회원정보변경
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/my/userModify")
     public String myUserModify() {
         return "pages/user/myUserModify";
     }
 
     // 마이페이지  > 회원정보변경 > 비밀번호변경
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/my/userPasswd")
     public String myUserPasswd() {
         return "pages/user/myUserPasswd";
     }
 
     // 마이페이지  > 회원정보변경 > 데이터업로더 팝업
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/popup/uploader")
     public String popupUploader() {
         return "pages/user/popUploader";

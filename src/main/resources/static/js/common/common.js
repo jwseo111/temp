2ca004a165a121e6b4fcc3304661cbcb2d7bf8fc
leesunmin83 <awear83@gmail.com>
2021-08-13 10:37:54 +0900

    loading bar 기능 추가

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -17,6 +17,7 @@ window.addEventListener('load', function() {
         headers: { 'X-Custom-Header': 'Custom' },
         timeout: 10000,
     });
+
 });
 
 function get(tid, uri, params, callback){
@@ -30,6 +31,7 @@ function get(tid, uri, params, callback){
         })
         .catch(function (error) {
             // 응답 실패
+
             if (error.response) {
                 // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
                 callback(tid, error.response.data);
@@ -44,15 +46,18 @@ function get(tid, uri, params, callback){
                 // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
                 // console.log('Error', error.message);
             }
+
             // console.log(error.config);
         });
 }
 
 function post(tid, uri, params, callback){
+    openLoading();
     axios.post(uri, params)
         .then(function (response) {
             // 응답 성공
             callback(tid, response.data);
+            closeLoading();
         })
         .catch(function (error) {
             // 응답 실패
@@ -70,6 +75,7 @@ function post(tid, uri, params, callback){
                 // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
                 // console.log('Error', error.message);
             }
+            closeLoading();
             // console.log(error.config);
         });
 }
@@ -372,4 +378,12 @@ function openGuide(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function openLoading(){
+    fnOpenPopup('loadingModal');
+}
+
+function closeLoading(){
+    fnClosePopup('loadingModal');
 }
